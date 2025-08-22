14c5df3a129713589ad16d4acbd5eb4d290d367e
hiverkiya <hiverkiya@gmail.com>
2025-08-22 17:46:07 +0530

    Added the convex package and configured it

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,14 +1,19 @@
+"use client"
 import { Button } from "@workspace/ui/components/button"
-import {Input} from "@workspace/ui/components/input";
+import {add} from "@workspace/math/add"
+import {useQuery} from "convex/react"
+import {api} from "@workspace/backend/_generated/api";
 
 export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-          <Input />
-      </div>
-    </div>
-  )
+    const users=useQuery(api.users.getMany);
+    return (
+        <div className="flex items-center justify-center min-h-svh">
+            <div className="flex flex-col items-center justify-center gap-4">
+                <h1 className="text-2xl font-bold">Hello World/widget</h1>
+                <Button size="sm">Button</Button>
+                <p>{add(2,4)}</p>
+                <p>Users are {JSON.stringify(users)}</p>
+            </div>
+        </div>
+    )
 }
---
+++ packages/backend/convex/schema.ts
@@ -0,0 +1,7 @@
+import {v} from "convex/values"
+import {defineSchema, defineTable} from "convex/server";
+export default defineSchema({
+    users: defineTable({
+        name:v.string(),
+    })
+})
