f06b1b8bb335860f2909cd8232ede4bb58051509
hiverkiya <hiverkiya@gmail.com>
2025-09-08 21:53:39 +0530

    Implemented the Clerk Authentication

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,21 +3,30 @@ import { Button } from "@workspace/ui/components/button"
 import {add} from "@workspace/math/add"
 import {useMutation, useQuery} from "convex/react"
 import {api} from "@workspace/backend/_generated/api";
+import {Authenticated, Unauthenticated} from "convex/react";
+import {SignInButton,UserButton} from "@clerk/nextjs";
 
 export default function Page() {
     const users=useQuery(api.users.getMany);
     const addUser=useMutation(api.users.add)
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World/web</h1>
-        <Button size="sm">Button</Button>
-          <p>{add(2,4)}</p>
+  return (<>
+          <Authenticated>
+              <div className="flex items-center justify-center min-h-svh">
+                  <div className="flex flex-col items-center justify-center gap-4">
+                      <h1 className="text-2xl font-bold">Hello World/web</h1>
+                      <UserButton/>
+                      <Button size="sm">Button</Button>
+                      <p>{add(2,4)}</p>
 
-          <Button onClick={()=>addUser()}>Add</Button>
-      </div>
-<div className="flex flex-col items-center justify-center min-h-svh"><p className="max-w-sm w-full mx-auto">Users are {JSON.stringify(users)}</p></div>
+                      <Button onClick={()=>addUser()}>Add</Button>
+                  </div>
+                  <div className="flex flex-col items-center justify-center min-h-svh"><p className="max-w-sm w-full mx-auto">Users are {JSON.stringify(users)}</p></div>
 
-    </div>
+              </div></Authenticated>
+          <Unauthenticated>
+              <p> Must be signed in</p>
+              <SignInButton>Sign in!</SignInButton>
+          </Unauthenticated>
+      </>
   )
 }
--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -2,12 +2,17 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import {ConvexProviderWithClerk} from "convex/react-clerk";
+import {useAuth, ClerkProvider} from '@clerk/nextjs'
 import {ConvexProvider, ConvexReactClient} from "convex/react";
+if(!process.env.NEXT_PUBLIC_CONVEX_URL){
+  throw new Error('Missing NEXT_PUBLIC_CONVEX_URL in your .env file')
+}
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL||"")
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ConvexProvider client={convex}>
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
       {children}
-    </ConvexProvider>
+    </ConvexProviderWithClerk>
   )
 }
