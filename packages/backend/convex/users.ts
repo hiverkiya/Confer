3fc53ccd606dc443053fdd00cf438b877898b62e
hiverkiya <hiverkiya@gmail.com>
2025-09-09 21:58:39 +0530

    Adding an organization field in the JWT template on Clerk

--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -1,23 +1,27 @@
-import {query, mutation} from "./_generated/server"
+import { query, mutation } from "./_generated/server";
 export const getMany = query({
-    args:{},
-    handler:async (ctx)=>{
-        const users=await ctx.db.query("users").collect();
-        return users;
+  args: {},
+  handler: async (ctx) => {
+    const users = await ctx.db.query("users").collect();
+    return users;
+  },
+});
 
+export const add = mutation({
+  args: {},
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (identity === null) {
+      throw new Error("Not authenticated");
     }
-})
+    const orgId = identity.orgId as string;
+    if (!orgId) {
+      throw new Error("Missing organization");
+    }
 
-export const add=mutation({
-    args:{},
-    handler: async (ctx)=>{
-        const identity= await ctx.auth.getUserIdentity()
-if(identity===null){
-    throw new Error("Not authenticated");
-}
-        const userId=await ctx.db.insert("users",{
-            name:"John"
-        })
-        return userId
-    }
-})
+    const userId = await ctx.db.insert("users", {
+      name: "John",
+    });
+    return userId;
+  },
+});
