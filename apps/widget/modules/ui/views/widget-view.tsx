9bf8334009109a466197b9f619522dd91569f09a
hiverkiya <hiverkiya@gmail.com>
2025-09-21 21:09:40 +0530

    Creating the Widget atoms and widget structure

--- apps/widget/modules/ui/views/widget-view.tsx
+++ apps/widget/modules/ui/views/widget-view.tsx
@@ -2,14 +2,27 @@
 import { WidgetFooter } from "@/modules/ui/components/widget-footer";
 import { WidgetHeader } from "@/modules/ui/components/widget-header";
 import { WidgetAuthScreen } from "@/modules/ui/screens/widget-auth-screen";
+import { useAtomValue } from "jotai";
+import { screenAtom } from "@/modules/widget/atoms/widget-atoms";
 
 interface Props {
   organizationId: string;
 }
 export const WidgetView = ({ organizationId }: Props) => {
+  const screen = useAtomValue(screenAtom);
+  const screenComponents = {
+    error: <p>TODO: Error</p>,
+    loading: <p>TODO: Loading</p>,
+    auth: <WidgetAuthScreen />,
+    voice: <p>TODO: Voice</p>,
+    inbox: <p>TODO: Inbox</p>,
+    selection: <p>TODO: Selection</p>,
+    chat: <p>TODO: Chat</p>,
+    contact: <p>TODO: Contact</p>,
+  };
   return (
     <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetAuthScreen />
+      {screenComponents[screen]}
     </main>
   );
 };
