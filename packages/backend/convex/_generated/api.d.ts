1c4fbdc482f04a880db04b269eba3178648399b4
hiverkiya <hiverkiya@gmail.com>
2025-09-22 22:27:01 +0530

    Updated the schema

--- packages/backend/convex/_generated/api.d.ts
+++ packages/backend/convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as public_contactSessions from "../public/contactSessions.js";
+import type * as public_conversations from "../public/conversations.js";
 import type * as users from "../users.js";
 
 /**
@@ -26,6 +27,7 @@ import type * as users from "../users.js";
  */
 declare const fullApi: ApiFromModules<{
   "public/contactSessions": typeof public_contactSessions;
+  "public/conversations": typeof public_conversations;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
