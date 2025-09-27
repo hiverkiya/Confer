1663bdb0aea0859634976782efa823dfca6bb0eb
hiverkiya <hiverkiya@gmail.com>
2025-09-27 17:26:29 +0530

    Implemented the AI tool calling functionality, yet to fix minor bugs

--- packages/backend/convex/private/messages.ts
+++ packages/backend/convex/private/messages.ts
@@ -2,8 +2,49 @@ import { action, mutation, query } from "../_generated/server";
 import { ConvexError, v } from "convex/values";
 import { components, internal } from "../_generated/api";
 import { supportAgent } from "../system/ai/agents/supportAgent";
+import { generateText } from "ai";
 import { paginationOptsValidator } from "convex/server";
 import { saveMessage } from "@convex-dev/agent";
+
+export const enhanceResponse = action({
+  args: {
+    prompt: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (identity == null) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Identity not found",
+      });
+    }
+    const orgId = identity.orgId as string;
+    if (!orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Organization not found",
+      });
+    }
+
+    const response = await generateText({
+      model: openai("gpt-4o-mini"),
+      messages: [
+        {
+          role: "system",
+          content:
+            "Enhance the operator's message to be more professional,clear, and helpful while maintaining their intent and key information",
+        },
+        {
+          role: "user",
+          content: args.prompt,
+        },
+      ],
+    });
+    return response.text;
+  },
+});
+
 export const create = mutation({
   args: {
     prompt: v.string(),
